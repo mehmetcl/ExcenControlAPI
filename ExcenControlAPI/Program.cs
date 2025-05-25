cde1702a4a3b94e7d39e0d9ff2b272161fb1c08e
mehmetcl <dev.mehmetcil@gmail.com>
2025-05-25 21:13:35 +0300

    Swagger tüm ortamlarda aktif ve root'a yönlendirildi

--- ExcenControlAPI/Program.cs
+++ ExcenControlAPI/Program.cs
@@ -1,29 +1,24 @@
-using ExcenControlAPI.Services;
+﻿using ExcenControlAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddSingleton<ILedService, LedService>();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ExcenControlAPI v1");
+    c.RoutePrefix = string.Empty; // ← Bu, https://excencenter.online
+});
 
 //app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
